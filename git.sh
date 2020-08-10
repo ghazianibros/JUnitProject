1155bc94173e94dc922036532110a63c9a8d379b
Muhammad Imran <mimran@ip-192-168-254-11.ec2.internal>
2020-08-10 15:38:09 -0500

    updated

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
 git add *
 git commit -m "$1"
-git remote add origin https://github.com/ghazianibros/helloworld-maven.git
+git remote add origin https://github.com/ghazianibros/JUnitProject.git
 git push -u origin master
